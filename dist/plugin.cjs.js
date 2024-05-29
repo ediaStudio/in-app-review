97ab9fca4022fab1aef3139f6df452105c0ae85f
wahid <sawyer179@yahoo.fr>
2024-05-29 09:49:57 +0700

    5

---
+++ dist/plugin.cjs.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('@capacitor/core');
+
+const InAppReview = core.registerPlugin('InAppReview', {
+    web: () => Promise.resolve().then(function () { return web; }).then(m => new m.InAppReviewWeb()),
+});
+
+class InAppReviewWeb extends core.WebPlugin {
+    async requestReview() {
+        throw this.unimplemented('Not implemented on web.');
+    }
+}
+
+var web = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    InAppReviewWeb: InAppReviewWeb
+});
+
+exports.InAppReview = InAppReview;
+//# sourceMappingURL=plugin.cjs.js.map
---
+++ dist/plugin.js
@@ -0,0 +1,26 @@
+var capacitorInAppReview = (function (exports, core) {
+    'use strict';
+
+    const InAppReview = core.registerPlugin('InAppReview', {
+        web: () => Promise.resolve().then(function () { return web; }).then(m => new m.InAppReviewWeb()),
+    });
+
+    class InAppReviewWeb extends core.WebPlugin {
+        async requestReview() {
+            throw this.unimplemented('Not implemented on web.');
+        }
+    }
+
+    var web = /*#__PURE__*/Object.freeze({
+        __proto__: null,
+        InAppReviewWeb: InAppReviewWeb
+    });
+
+    exports.InAppReview = InAppReview;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+    return exports;
+
+})({}, capacitorExports);
+//# sourceMappingURL=plugin.js.map
